d8c841902aa1ecaff6147da47885d2b77bbb16ba
Luiz Fernando Noschang <noschang@univali.br>
2014-10-01 18:32:17 -0300

    Adicionando metodos na interface do plugin para destaque de codigo fonte e exibicao de painel flutuante

--- src/br/univali/ps/plugins/base/UtilizadorPlugins.java
+++ src/br/univali/ps/plugins/base/UtilizadorPlugins.java
@@ -1,6 +1,8 @@
 package br.univali.ps.plugins.base;
 
 import javax.swing.Action;
+import javax.swing.JComponent;
+import javax.swing.JPanel;
 
 /**
  *
@@ -13,6 +15,12 @@ public interface UtilizadorPlugins
     public void instalarAcaoPlugin(Plugin plugin, Action acao);
     
     public String obterCodigoFonteUsuario();
+    
+    public void destacarTrechoCodigoFonte(int linha, int coluna, int tamanho);
+    
+    public void exibirPainelFlutuante(JComponent origem, JPanel conteudo, boolean painelOpaco);
+    
+    public void ocultarPainelFlutuante();
     
     public void desinstalarAcaoPlugin(Plugin plugin, Action acao);
     
